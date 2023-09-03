1dae12ce5746dbf834093e0ecb16ec78b68f84f8
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-09-03 20:01:17 +0600

    project init

---
+++ TextComponents/TextComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextComponentsApp.swift
+//  TextComponents
+//
+//  Created by Shah Md Imran Hossain on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct TextComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
